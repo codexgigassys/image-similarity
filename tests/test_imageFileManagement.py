ca1e2bdd4806912814c16140e40ba8c666fb8d7b
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-06-16 14:43:16 -0300

    Files Updated

--- tests/test_imageFileManagement.py
+++ tests/test_imageFileManagement.py
@@ -1,17 +1,20 @@
 import sys
-import cv2
 import os
 sys.path.append('../')
 import imageSimilarity
+from imageSimilarity import Image
 sys.path.append('tests')
 
 def test_batchOfFilesIsSavedInSpecifiedPath():
-    images = imageSimilarity.imagesInPath("testImages")
+    images = Image.allFromPath("testImages")
     batchs = imageSimilarity.similarImagesDividedInLists(images,0.95)
     imageSimilarity.saveImagesInLists(batchs,'testResult')
     assert len(os.listdir('testResult')) == 5
 
+def test_imageSHA1HashReturnExpectedValueFromBuffer():
+    anImageFile = Image.fromBuffer(open('testImages/06.jpeg','rb').read())
+    return anImageFile.sha1() == "4dedb7caab44c5b65ba0692cc54a5f2735dee5f4"
 
-def test_imageHashReturnExpectedValue():
-    anImageFile = open('testImages/06.jpeg','rb')
-    return imageSimilarity.hashBuffer(anImageFile.read()) == "4dedb7caab44c5b65ba0692cc54a5f2735dee5f4"
+def test_imageSHA1HashReturnExpectedValueFromPathFile():
+    anImageFile = Image.fromPath('testImages/06.jpeg')
+    return anImageFile.sha1() == "4dedb7caab44c5b65ba0692cc54a5f2735dee5f4"
