817041cd665d845deb7f3089e324a041e11a505a
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-08-17 14:48:56 -0300

    IS-6 PEP8 fixed

--- exampleCode.py
+++ exampleCode.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
+
 filesDictionary = {
     '01.jpeg': open('images/01.jpeg', 'rb'),
     '02.jpeg': open('images/02.jpeg', 'rb'),
     '05.jpeg': open('images/05.jpeg', 'rb'),
 }
 
-response = requests.post("http://localhost:9082/api/imageSimilarityByHash",files=filesDictionary,params={'similarity_grade':0.75})
+response = requests.post("http://localhost:9082/api/imageSimilarityByHash", files=filesDictionary, params={'similarity_grade': 0.75})
 
 hashes = json.loads(response.text)['lists']
 print(hashes)
--- tests/test_imageSimilarities.py
+++ tests/test_imageSimilarities.py
@@ -5,44 +5,44 @@ sys.path.append('../')
 import imageSimilarity
 from imageSimilarity import Image
 
-testImages = Image.allFromPath('testImages')
+testImages = Image.allFromPath('/image-similarity/tests/testImages/')
 
 def test_twoEqualImagesHasSimilarityOne():
 
-    imageOne = Image.fromPath('testImages/01.jpeg')
-    imageTwo = Image.fromPath('testImages/01.jpeg')
+    imageOne = Image.fromPath('tests/testImages/01.jpeg')
+    imageTwo = Image.fromPath('tests/testImages/01.jpeg')
 
     result = imageOne.similarityWith(imageTwo)
 
     assert result == 1
 
 def test_twoNotSimilarImagesHasLowSimilarity():
-    imageOne = Image.fromPath('testImages/01.jpeg')
-    imageTwo = Image.fromPath('testImages/03.jpeg')
+    imageOne = Image.fromPath('tests/testImages/01.jpeg')
+    imageTwo = Image.fromPath('tests/testImages/03.jpeg')
 
     result = imageOne.similarityWith(imageTwo)
 
     assert result < 0.25
 
 def test_twoSimilarImagesHasHighSimilarity():
-    imageOne = Image.fromPath('testImages/01.jpeg')
-    imageTwo = Image.fromPath('testImages/02.jpeg')
+    imageOne = Image.fromPath('tests/testImages/01.jpeg')
+    imageTwo = Image.fromPath('tests/testImages/02.jpeg')
 
     result = imageOne.similarityWith(imageTwo)
 
     assert result > 0.9
 
 def test_fiveBatchsFromTestImagesWithHighSimilarity():
-    batchs = imageSimilarity.similarImagesDividedInLists(testImages,0.95)
+    batchs = imageSimilarity.similarImagesDividedInLists(testImages, 0.95)
     quantityOfBatchs = len(batchs)
     assert quantityOfBatchs == 5
 
 def test_twentyOneBatchsFromTestImagesWithSimilarityOne():
-    batchs = imageSimilarity.similarImagesDividedInLists(testImages,1)
+    batchs = imageSimilarity.similarImagesDividedInLists(testImages, 1)
     quantityOfBatchs = len(batchs)
     assert quantityOfBatchs == 21
 
 def test_twentyOneBatchsFromTestImagesWithSimilarityOne():
-    batchs = imageSimilarity.similarImagesDividedInLists(testImages,0)
+    batchs = imageSimilarity.similarImagesDividedInLists(testImages, 0)
     quantityOfBatchs = len(batchs)
     assert quantityOfBatchs == 1
