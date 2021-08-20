064369d5b459f7a64571c2f93ed3f203acc8cd22
Lucas Ariel Saavedra <lucasarielsaavedra@hotmail.com>
2021-08-20 13:40:45 -0300

    IS-7 config.yml updated

--- tests/testing_configuration.py
+++ tests/testing_configuration.py
@@ -1,4 +1,4 @@
 import logging
 
 
-logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s][%(levelname)s] %(message)s', filename='imageSimilarity.log')
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s][%(levelname)s] %(message)s', filename='/image-similarity/imageSimilarity.log')
